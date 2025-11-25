7b9f43d3711b2d9c477304a3eff84061419c58bd
dxgtnnn <davy.gatineau@epitech.eu>
2025-11-25 13:39:54 +0100

    [UPDATE] code

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -6,18 +6,19 @@ void GUI::run()
     const int cellSize = 10;
     int width = 0;
     int height = 0;
+    bool paused = false;
     string answer = "";
     string path = "";
+
     cout << "Dimensions ([largeur]x[hauteur]): ";
     cin >> answer;
     path = "input/" + answer + ".txt";
+    RenderWindow window(VideoMode(1920, 1080), "Game of Life");
     ifstream file(path);
     file >> width >> height;
     file.close();
-    RenderWindow window(VideoMode(1920, 1080), "Game of Life");
     Grid grid(width, height, cellSize);
     grid.initialize(path);
-    bool paused = false;
 
     while (window.isOpen()) {
         Event event;
